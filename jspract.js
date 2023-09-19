23968de3f38af0b384d35ccb99248e02f74c65db
Demian Zolotukhin <demianzolotukhin@gmail.com>
2023-09-19 20:04:43 +0300

    09/19/23

---
+++ jspract.js
@@ -0,0 +1,13 @@
+const x = "Hello world";
+console.log(x);
+
+
+let processed = 0;
+
+function processArg(num) {
+  return (num + 8) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
+console.log(processed);
