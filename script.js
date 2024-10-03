4242478fa67ed538e7224f197df80df50e039c93
Demian Zolotukhin <demianzolotukhin@gmail.com>
2024-10-03 18:46:33 +0300

    forEach

---
+++ script.js
@@ -0,0 +1,358 @@
+/* function printFromTo(input, start, end) {
+    debugger;
+  for (let i = start; i <= end; i++) {
+    console.log(input[i])
+  }
+}
+
+printFromTo('datatransmitter', 0, 5)
+
+
+function isWerewolf(target) {
+    debugger;
+    let direct = '';
+    let reversed = '';
+  
+    for (let ch of target) {
+      if (!' ,'.includes(ch)) {
+        direct += ch;
+        reversed = ch + reversed;
+      }
+    }
+  
+    console.log(direct.toLowerCase() === reversed.toLowerCase());
+  }
+
+  isWerewolf('Danetski aeroplan')
+
+
+  const words = ['one', 'two', 'three', 'four', 'five'];
+
+  function indexOf(items, itemToFind) {
+
+    for (let i = 0; i <= items.length; i++) {
+      if (items[i] === itemToFind) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
+  console.log(
+    indexOf(words, 'four')
+    )
+
+
+    function makeDecision(fuelRemaining, distance, fuelConsumption) { 
+      debugger;
+      if (fuelRemaining < 0 || distance < 0 ||
+      fuelConsumption <= 0) {
+        return 'please, enter the valid data';
+      }
+    
+      let fuelPerKm = fuelConsumption / 100;
+      let toDrive = distance * fuelPerKm;
+      if (fuelRemaining < toDrive) {
+        return 'ask for help';
+      }
+    
+      if (fuelRemaining >= toDrive) {
+        return 'reach gas station by themselves';
+      }
+    }
+
+    makeDecision(20, 103, 19)
+
+
+    function getLengthOfNumbers(num) {
+      let sum = 0;
+
+      debugger;
+
+      for (let i = num; i > 0; i--) {
+        console.log(i)
+      }
+    }
+
+    getLengthOfNumbers(89)
+
+    function isWerewolf(target) {
+      let toLower = target.toLowerCase()
+      let standart = '';
+      let reverse = '';
+      debugger;
+    
+      for (let ch of toLower) {
+        if ('abcdefghijklmnopqrstuvwxyz'.includes(ch))
+        reverse = ch + reverse,
+        standart += ch;
+      }
+    
+      return standart === reverse
+    }
+  
+  isWerewolf('red rum sir is murder')
+
+
+  function splitString(str) {
+    let corrected = str;
+    let result = [];
+  
+    debugger;
+
+    if (corrected.length % 2 !== 0) {
+      corrected += '_'
+    }
+  
+    for (let i = 0; i < corrected.length; i += 2) {
+      result.push(corrected[i] + corrected[i + 1])
+    }
+  
+    return result;
+  }
+  
+  splitString('ab cd ef');
+
+
+  let str1 = [];
+
+ for (let i = 1; i < 256; i++) {
+
+  str1 += String.fromCodePoint(i)
+  console.log(String.fromCodePoint(i))
+ }
+ console.log(str1)
+
+ let x = 1234567
+
+ console.log(String(x).length)
+
+
+ function calculateGuests(guestsInput) { // 'str2 trg 5s'
+  let digits = '';
+  debugger;
+
+  for (const ch of guestsInput) { // 'str24 trg 5s'
+    if ('0123456789'.includes(ch)) { // 
+      digits += ch; // 24
+      continue;
+    }
+
+    if (digits.length > 0) {
+      // we already have a number we need
+      break;
+    }
+  }
+
+  console.log(+digits || 'not a number');
+}
+
+calculateGuests('str24 trg 5s')
+
+
+function getName(count) {
+  switch (true) {
+    case count < 2:
+      return 'One';
+    
+    case count <= 4:
+      return 'A few';
+
+    default:
+      return 'Many';
+  }
+}
+
+
+function getLeaders(numbers) {
+  const result = [];
+  let sum = 0;
+
+  for (let i = numbers.length - 1; i >= 0; i--) {
+    const n = numbers[i];
+
+    if (n > sum) {
+      result.push(n);
+    }
+
+    sum += n;
+  }
+
+  // numbers should go in the original order
+  return result.reverse();
+}
+
+console.log(getLeaders([15, 35 , 12, 4, 2, 5, 0]))
+
+function getKiller(suspects, dead) {
+  debugger;
+
+  for (let suspect of Object.values(suspects)) { //['Jacob', 'Bill', 'Lucas']
+
+    for (let person of suspect) {
+
+      let count = 0;
+      
+      for (let victim of dead) {
+        if (person === victim) {
+          count++;
+        }
+      }
+
+      if (count > 2) {
+        return suspects[suspect];
+      }
+    }
+  }
+}
+
+
+
+console.log(getKiller({
+  'James': ['Jacob', 'Bill', 'Lucas'],
+  'Johnny': ['David', 'Kyle', 'Lucas'],
+  'Peter': ['Lucy', 'Kyle']
+}, ['Lucas', 'Bill']))
+
+
+function getUserInfo() {
+  console.log(`Hi ${this.username}, happy ${this.age}`);
+}
+
+const user1 = {
+  userName: 'Alex',
+  age: 32,
+  partner: false,
+  userGet: getUserInfo,
+}
+
+user1.userGet();
+
+const user2 = {
+  userName: 'Alex',
+  lastName: 'Turner',
+  partner: false,
+  get fullName() {
+    return `${this.userName} ${this.lastName}`
+  },
+}
+
+
+console.log(user2.fullName);
+
+const user3 = {
+  firstName: 'John',
+  lastName: 'Smith',
+  
+  set fullName(value) {
+    const trim = value.trim();
+    const arr = trim.split(' '); // знаходимо перший пробіл
+
+    if (arr.length < 2) {
+      this.firstName = 'za',
+      this.lastName = 'loopa';
+    } else {
+    this.firstName = arr[0]; // Ім'я має бути першим
+    this.lastName = arr[1]; // Прізвище - другим
+    }
+  },
+};
+
+console.log(user3.firstName, user3.lastName);
+
+user3.fullName = 'Demian ';
+
+console.log(user3.firstName, user3.lastName); */
+
+
+// const user6 = {
+//   firstName: 'Anton',
+//   lastName: 'Griban',
+//   age: 27,
+
+//   get fullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+
+//   set fullName(value) {
+//     if (typeof value !== 'string') {
+//       return;
+//     }
+
+//     if (!value.includes(' ')) {
+//       return;
+//     }
+
+//     let split = value.split(' ');
+
+//     if (split.length === 2) {
+//       this.firstName = split[0];
+//       this.lastName = split[1];
+//     }
+//   }
+// };
+
+// console.log(user6.fullName);
+
+// user6.fullName = 'Dana White';
+
+// console.log(user6.fullName);
+
+
+// const username = 'Danila';
+
+// console.log(
+//   username.split(''),
+//   [...username],
+//   Array.from(username),
+// )
+
+// const n = [2, 4, 4, 5, 2, 6, 8];
+// let a = ['x', 'y', 32, [4, 5]]
+// let [...numbers] = a[a.length -1] 
+// result4 = n.concat(54, a, 56)
+// console.log(result4);
+
+
+'use strict';
+
+const numbers = [3, 1, 12, 5, 2, 3, 4];
+let result;
+
+console.log(numbers);
+
+/*  Mutating methods */
+result = numbers.push(100, 200, 300);
+result = numbers.unshift(100, 200, 300);
+// result = numbers.pop();
+// result = numbers.shift();
+// result = numbers.splice(2, 3, 100, 200, 300, 400);
+// result = numbers.fill('a', 2, 5);
+// result = numbers.reverse();
+// result = numbers.sort();
+
+/* Non mutating methods */
+// result = numbers.includes(3);
+// result = numbers.indexOf(3);
+// result = numbers.lastIndexOf(3);
+// result = numbers.join('-');
+// result = numbers.slice(2, 5);
+result = numbers.concat(100, ['x', 'y', 999], 200, 300);
+
+console.log('---------');
+
+console.log(numbers);
+console.log(result);
+
+const wordsArr = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
+
+wordsArr.OnforEach = function(callback) {
+  console.log(this);
+
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i])
+  }
+}
+
+wordsArr.OnforEach((param) => console.log(`Hello people number ${param}`));
